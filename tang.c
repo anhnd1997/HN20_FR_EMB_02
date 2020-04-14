d32571934e4f4b1d0d26eb5bcfb356ca832236fe
anhnd48 <nguyenducanh29091997@gmail.com>
2020-04-14 16:07:15 +0700

    a file

---
+++ tang.c
@@ -0,0 +1,17 @@
+int tang(int a[],int n)
+{
+    int i,j;
+    int tg;
+    for(i=0;i<(n-1);i++)
+    {
+        for(j=(i+1);j<n;j++)
+        {
+            if(a[i] > a[j])
+            {
+                tg = a[i];
+                a[i] = a[j];
+                a[j] = tg;
+            }
+        }
+    }
+}
